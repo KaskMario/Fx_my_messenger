b4a1b9ad9f3bba31b692653c4c816f371fc26918
KaskMario <mariokask@gmail.com>
2024-04-14 20:17:10 +0200

    connect

---
+++ src/main/java/com/example/fx_my_messenger/HelloApplication.java
@@ -0,0 +1,36 @@
+package com.example.fx_my_messenger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        HelloController controller = fxmlLoader.getController();
+        controller.setStage(stage);
+        stage.setTitle("Messenger");
+        Image icon = new Image("send.png");
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        //stage.initStyle(StageStyle.DECORATED);
+        // stage.setY(50);
+        // stage.setX(50);
+        //stage.setFullScreen(true);
+        // stage.setFullScreenExitHint("No way out..  hahahahahahahaaa");
+        //stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
